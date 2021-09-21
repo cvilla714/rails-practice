2f21e5f9e6d16dd30686513204993d70921fed04
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-21 18:36:16 -0400

    adding validation to the model article, title and desciption  to be present and have fixed length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 10, maximum: 25 }
+  validates :description, presence: true, length: { minimum: 13, maximum: 430 }
 end
