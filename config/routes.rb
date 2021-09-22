6414456ff787704cf78ce31c321768c5f78b5ebe
Cosmel Villalobos <cosmelvg@gmail.com>
2021-09-22 01:22:49 -0400

    added the index method to resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get '/about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: %i[show index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
